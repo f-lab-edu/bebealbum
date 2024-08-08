9ef9cf4a6b6920badcb3d5e9809a5e662bdfff7c
jckang <jckang568@gmail.com>
2024-08-08 20:05:26 +0900

    #Chore: initial commit

---
+++ app/src/main/java/io/bebealbum/app/BebealbumApplication.java
@@ -0,0 +1,11 @@
+package io.bebealbum.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BebealbumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BebealbumApplication.class, args);
+    }
+}
