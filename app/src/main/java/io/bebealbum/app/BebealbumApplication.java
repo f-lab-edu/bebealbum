2612c9d443912b12b52ab243ab4482476dbcddee
jckang <jckang568@gmail.com>
2024-08-14 19:03:48 +0900

    #Chore: gradle 세팅 수정 중

--- app/src/main/java/io/bebealbum/app/BebealbumApplication.java
+++ app/src/main/java/io/bebealbum/app/BebealbumApplication.java
@@ -1,11 +1,18 @@
 package io.bebealbum.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BebealbumApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(BebealbumApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(BebealbumApplication.class, args);
+        logger.info("asdfasdfasdfasdfasdfasdf");
     }
 }
--- buildSrc/settings.gradle
+++ buildSrc/settings.gradle
@@ -3,7 +3,6 @@
  *
  * This settings file is used to specify which projects to include in your build-logic build.
  */
-
 dependencyResolutionManagement {
     // Reuse version catalog from the main build.
     versionCatalogs {
